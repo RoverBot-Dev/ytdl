7747faa21c1e4f61fd0103aae1f7761af68b6948
RoverBot-Dev <roverbotdev@noreply.com>
2021-01-24 14:51:57 +0300

    commit

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
-// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
-// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+
+
+
+require(['/node_modules/ytdl-core', '/node_modules/fs', '/node_modules/JSON'], function (foo) {
+    //foo is now loaded.
 });
 
-rl.question("youtube linki ver? ", function(url) {
-  rl.question("video adı ve uzantısı? ", function(ad) {
- 
-    ytdl(url ,{quality: "highestaudio"}).pipe(fs.createWriteStream(`${ad}`));
-console.log("Yükleniyor... kaydediliyor...")
-}    
-);  
-    
-}    
-);
+async function metadata(){
+    let url2 = document.getElementById("url").value
+if(ytdl.validateURL(url)){
+ let info = await ytdl.getInfo();
+ console.log(info)
+}        
 
-
+}
